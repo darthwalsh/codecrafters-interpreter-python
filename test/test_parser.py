d4963d61ee9be2a23a277e9cf33b9708cb779549
Carl Walsh <darthwalsh@gmail.com>
2025-02-13 17:06:19 -0800

    cleanup tests

--- test/test_parser.py
+++ test/test_parser.py
@@ -10,6 +10,15 @@ class TestParser(unittest.TestCase):
         self.assertEqual(errors, [])
         self.assertEqual(AstPrinter().print(expr), printed)
 
+    def error(self, source, error: str, parsed: str | None):
+        e = parse_stmt(source) if ";" in source else parse_expr(source)
+        self.assertEqual(errors, [error])
+
+        if parsed is None:
+            self.assertIsNone(e)
+        else:
+            self.assertEqual(AstPrinter().print(e), parsed)
+
     def test_primary(self):
         self.validate("1", "1.0")
         self.validate('"ab"', "ab")
@@ -38,13 +47,8 @@ class TestParser(unittest.TestCase):
         self.validate("x = y", "(= x y)")
         self.validate("x = y = z", "(= x (= y z))")
 
-        e = parse_expr("x =")
-        self.assertEqual(errors, ["Expect expression"])
-        self.assertIsNone(e)
-
-        e = parse_expr("1 = x")
-        self.assertEqual(errors, ["Invalid assignment target."])
-        self.assertEqual(AstPrinter().print(e), "1.0")
+        self.error("x =", "Expect expression", None)
+        self.error("1 = x", "Invalid assignment target.", "1.0")
 
     def test_var(self):
         self.validate("var x = 1 + x;", "var x = (+ 1.0 x);")
@@ -52,20 +56,11 @@ class TestParser(unittest.TestCase):
     def test_print(self):
         self.validate("print 1;", "print 1.0;")
         
-        e = parse_stmt("print 1; 3")
-        self.assertEqual(errors, ["Expect ';' after expression."])
-        self.assertEqual(AstPrinter().print(e), "print 1.0;")
-
-        e = parse_stmt("print; 3;")
-        self.assertEqual(errors, ["Expect expression"])
-        self.assertEqual(AstPrinter().print(e), "3.0;")
+        self.error("print 1; 3", "Expect ';' after expression.", "print 1.0;")
+        self.error("print; 3;", "Expect expression", "3.0;")
 
     def test_trailing(self):
-        e = parse_expr("1 1")
-        self.assertEqual(errors, ["Expected end of expression"])
-        self.assertEqual(AstPrinter().print(e), "1.0")
+        self.error("1 1", "Expected end of expression", "1.0")
 
     def test_trailing_after_parens(self):
-        e = parse_expr("(72 +)")
-        self.assertEqual(errors, ["Expect expression"])
-        self.assertIsNone(e)
+        self.error("(72 +)", "Expect expression", None)
